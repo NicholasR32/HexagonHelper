13ad9b3fb66266eeb215d08a50031ed86c3f59f1
NicholasR32 <98115878+NicholasR32@users.noreply.github.com>
2024-02-27 00:55:57 -0500

    Clean up code, polish timer scanner

--- scantimer.py
+++ scantimer.py
@@ -2,7 +2,7 @@ import pyautogui as pag
 from PIL import Image
 import time
 
-DEBUG = True
+DEBUG = False
 
 # Magic numbers
 last_x_percent = 0.555
@@ -24,6 +24,7 @@ frames_digit_height_percent = 0.045
 frames_x_percent = 0.857
 frames_y_percent = 0.374
 
+# Screenshot a region x% right and y% down on the screen, that is w% wide by h% tall.
 def screenshot_region(x, y, width, height):
     # Get display dimensions
     screen_width, screen_height = pag.size()
@@ -39,7 +40,9 @@ def screenshot_region(x, y, width, height):
 
     return screenshot
 
-def scan_for_last():
+# Constantly scan for the "LAST" text to know when the death screen is shown.
+def scan_for_last(records):
+    player_alive = True
     while True:
         # Delay screenshot calls to save resources
         time.sleep(0.1)
@@ -47,26 +50,38 @@ def scan_for_last():
 
         # Screenshot the region where the "LAST" text should be
         screenshot_last = screenshot_region(last_x_percent, last_y_percent, last_width_percent, last_height_percent)
-        screenshot_last.save("last.png")
+        # screenshot_last.save("last.png")
 
         # Check if the text is there
         try:
-            found = pag.locate(needleImage="last_text.png", haystackImage=screenshot_last)
+            found = pag.locate(needleImage="imgs/last_text.png", haystackImage=screenshot_last)
         # Not there.
         except pag.ImageNotFoundException:
             print("LAST text not found.\n" if DEBUG else "", end="")
+            player_alive = True
         # Text is there, grab the timer!
         else:
             print("LAST text found!\n" if DEBUG else "", end="")
-            # print(found)
-            scan_timer()
+            # Wait for the screen to fully load.
+            time.sleep(0.2)
+            record = scan_timer()
+
+            # Only add the record once per death screen.
+            if player_alive:
+                player_alive = False
+                print(f"Time survived: {record[0]}:{record[1]}")
+
+                records.append([record, time.strftime("%m/%d/%y %I:%M:%S %p", time.localtime())])
+                print(records)
+
             time.sleep(1)
 
 def scan_timer():
-    time = ""
+    time = []
     # Scan each seconds digit in the timer from L to R, starting with the thousands place.
     # 0=thousands, 1=hundreds, 2=tens, 3=ones
     # (If the thousands  digit ever gets used, I will be very impressed)
+    seconds_survived = ""
     for place in range(0,4):
         # Screenshot region where this digit should be.
         screenshot_digit = screenshot_region(place * seconds_digit_width_percent + thousands_x_percent,
@@ -77,13 +92,15 @@ def scan_timer():
         digit_val = locate_digit(screenshot_digit, True)
 
         if digit_val == -1:
-            print(f"seconds place {place} has no digit.")
+            print(f"seconds place {place} has no digit.\n" if DEBUG else "", end="")
         else:
-            time += str(digit_val)
+            seconds_survived += str(digit_val)
+            print(f"seconds place {place} has digit {digit_val}.\n" if DEBUG else "", end="")
 
-    time += ":"
+    time.append(seconds_survived)
 
     # Scan each frames digit in the timer. Easier, since there's always 2 of them.
+    frames_survived = ""
     for place in range(0,2):
         # Screenshot region where this digit should be.
         screenshot_digit = screenshot_region(place * frames_digit_width_percent + frames_x_percent,
@@ -95,10 +112,13 @@ def scan_timer():
 
         # This should never happen.
         if digit_val == -1:
-            print(f"frames place {place} has no digit!?!?!?!?")
+            print(f"frames place {place} has no digit!?!?!?!?\n" if DEBUG else "", end="")
         else:
-            time += str(digit_val)
-    print(f"Time survived: {time}")
+            frames_survived += str(digit_val)
+            print(f"frames place {place} has digit {digit_val}.\n" if DEBUG else "", end="")
+    time.append(frames_survived)
+
+    return time
 
 # Searches the given screenshot for any digit.
 def locate_digit(screenshot_digit, seconds_or_frames):
@@ -107,17 +127,16 @@ def locate_digit(screenshot_digit, seconds_or_frames):
         try:
             # Compare with the digit templates in either seconds or frames places.
             if seconds_or_frames:
-                found = pag.locate(needleImage=f"secondsdigits/{digit}_seconds.png",
+                found = pag.locate(needleImage=f"imgs/secondsdigits/{digit}_seconds.png",
                                    haystackImage=screenshot_digit,
                                    confidence=0.95)
             else:
-                found = pag.locate(needleImage=f"framesdigits/{digit}_frames.png",
+                found = pag.locate(needleImage=f"imgs/framesdigits/{digit}_frames.png",
                                     haystackImage=screenshot_digit,
                                     confidence=0.95)
         # This is not the digit.
         except pag.ImageNotFoundException:
             pass
-            # print(f"{digit} not in place {place}.")
         # This digit is here, return it!
         else:
             return digit
@@ -125,7 +144,10 @@ def locate_digit(screenshot_digit, seconds_or_frames):
     # There is no digit in this spot. Should only happen for the tens, hundreds, and thousands of seconds.
     return -1
 
-scan_for_last()
+if __name__ == '__main__':
+    records = []
+    scan_for_last(records)
+
 
 
 
